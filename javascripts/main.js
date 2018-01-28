5627054a1d9e56a89595027252c937b55ecc5847
Eduardo DuQuesne <eduquesne@eduardos-mbp.attlocal.net>
2018-01-27 21:57:45 -0600

    Recall Settings

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,33 +3,5 @@
 // const $ = require('jquery');
 require('./dom-interactions');
 
-// let storeSettings = () => {
-//     let settings = {
-//     uid: firebase.auth().currentUser.uid,
-//     tempo: Tone.Transport.bpm.value,
-//     tempoDisplay: tempoKnob.value,
-//     arpMatrix: sequencer.matrix.pattern,
-//     arpPan: arpVolPan.pan.value,
-//     arpPanDisplay: arpVolPanKnob.x,
-//     arpVol: arpVolPan.volume.input.value,
-//     arpVolDisplay: arpVolPanKnob.y,
-//     bassMatrix: bassSequencer.matrix.pattern,
-//     bassPan: bassVolPan.pan.value,
-//     bassPanDisplay: bassVolPanKnob.x,
-//     bassVol: bassVolPan.volume.input.value,
-//     bassVolDisplay: bassVolPanKnob.y,  
-//     beatMatrix: beatSequencer.matrix.pattern,
-//     beatPan: beatVolPan.pan.value,
-//     beatPanDisplay: beatVolPanKnob.x,
-//     beatVol: beatVolPan.volume.input.value,
-//     beatVolDisplay: beatVolPanKnob.y,  
-//     delayOneWet: delayOne.wet.value, 
-//     delayOneDisplay: dialDelayWet.value,
-//     delayOneFB: delayOne.feedback.value,
-//     delayOneTime: delayOne.delayTime.value,
-//     delayOneTimeDisplay: dialDelayTime.value,
-//     delayOneFBDisplay: dialDelayFeedback.value
-//     };
-//     return settings;
-// };
+
 
